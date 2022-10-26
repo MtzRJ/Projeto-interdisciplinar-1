9b15b2ff7866b48c65c51d63bd3ae31886a591f8
mtzcrpt@gmail.com <mtzcrpt@gmail.com>
2022-10-25 21:52:35 -0300

    modified:   index.html

---
+++ gravarDados.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RoadTrip</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+</head>
+<body>
+	<h2>Seja Bem-vindo(a)</h2>
+	<?php
+    $Usuario = $_POST["user"];
+    $password = $_POST["password"];
+    echo "<br/>Usuario: " .$user;
+    echo "<br/>password: " .$password;    
+?>
+</body>
+</html>
